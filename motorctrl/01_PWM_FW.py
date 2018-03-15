0d40d0294499891fc43fb28b1f39632440bc7092
Jvaneyll <jonathanvaneyll@yahoo.fr>
2018-03-15 22:05:45 +0100

    Modifications following first hardware tests and re-assignment of pins to correct motors. Need to evolve the movements to allow simultaneous movement of motors.

--- motorctrl/01_PWM_FW.py
+++ motorctrl/01_PWM_FW.py
@@ -6,12 +6,19 @@ GPIO.setwarnings(False)
 
 #init board
 GPIO.setmode(GPIO.BOARD)
-R_PWM_pin=int(12)
-R_FW_pin=int(16)
-R_RV_pin=int(18)
-L_PWM_pin=int(33)
-L_FW_pin=int(15)
-L_RV_pin=int(37)
+L_PWM_pin=int(12)
+L_FW_pin=int(16)
+L_RV_pin=int(18)
+R_PWM_pin=int(33)
+R_FW_pin=int(37)
+R_RV_pin=int(15)
+
+#R_PWM_pin=int(12)
+#R_FW_pin=int(16)
+#R_RV_pin=int(18)
+#L_PWM_pin=int(33)
+#L_FW_pin=int(15)
+#L_RV_pin=int(37)
 
 #init I/O
 
@@ -23,8 +30,8 @@ GPIO.setup(outpin, GPIO.OUT)
 
 #PWM pins
 ##Initialize PWM frequency variable
-PWM_freq=int(10000)
-launch_time=int(0.3)
+PWM_freq=int(5000)
+launch_time=0.1
 
 ##Set PWM pins
 r = GPIO.PWM(R_PWM_pin, PWM_freq)
@@ -44,9 +51,9 @@ REV = 3
 
 def Inactive():
 	""" Deactivate H bridge """
-	GPIO.output(R_PWM_pin, GPIO.LOW )
+	GPIO.output(R_PWM_pin, GPIO.LOW)
 	if(L_PWM_pin != R_PWM_pin):
-		GPIO.output(L_PWM_pin, GPIO.LOW )
+		GPIO.output(L_PWM_pin, GPIO.LOW)
 
 def RActive(Rdc):
 	""" Activate H bridge """
@@ -56,9 +63,9 @@ def RActive(Rdc):
 	r.start(dc)
 	time.sleep(launch_time)
 	#change the duty cycle to desired one
-	Rdc=50
+	#Rdc=50
 	r.ChangeDutyCycle(Rdc)  # where 0.0 <= Rdc <= 100.0
-	time.sleep(10)
+	time.sleep(5)
 	
 def LActive(Ldc):
 	l = GPIO.PWM(L_PWM_pin, PWM_freq)
@@ -67,9 +74,9 @@ def LActive(Ldc):
 	l.start(dc)
 	time.sleep(launch_time)
 	#change the duty cycle to desired one
-	Ldc=50
+	#Ldc=50
 	l.ChangeDutyCycle(Ldc)
-	time.sleep(10)
+	time.sleep(5)
 
 def RMotDir(rot):
 	""" Define rotation direction for right motor """
@@ -98,13 +105,27 @@ def LMotDir(rot):
 if __name__ == '__main__':
 	Inactive()
 	RMotDir(2)
+	LMotDir(2)
 	RActive(50)
+	LActive(50)
 	Inactive()
 	RMotDir(3)
+	LMotDir(3)
 	RActive(50)
-	Inactive()
+	LActive(50)
 	RMotDir(1)
+	time.sleep(1)
+	LMotDir(1)
+	Inactive()
+	RMotDir(2)
+	LMotDir(3)
 	RActive(50)
+	LActive(50)
+	Inactive()
+	RMotDir(3)
+	LMotDir(2)
+	RActive(50)
+	LActive(50)
 	Inactive()
 	GPIO.cleanup()
 		
